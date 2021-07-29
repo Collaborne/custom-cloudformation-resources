90041f26a19f5d9e683d793b4fcf2eabcb27a5d4
Andreas Kohn <andreas.kohn@gmail.com>
2021-07-29 21:34:15 +0200

    Simplify the expected type of logger
    Instead of expecting an arbitrary amount of arguments, we now only expect a single one
    that contains the needed values. This allows an implementation more freedom in terms
    of rendering these values, and also makes translating error message _formats_ theoretically
    possible.

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,6 @@
 export interface Logger {
-	log(message: string, ...args: any[]): void;
-
-	debug(message: string, ...args: any[]): void;
-	warn(message: string, ...args: any[]): void;
-	error(message: string, ...args: any[]): void;
+	log(message: string, args?: Record<string, unknown>): void;
+	debug(message: string, args?: Record<string, unknown>): void;
+	warn(message: string, args?: Record<string, unknown>): void;
+	error(message: string, args?: Record<string, unknown>): void;
 }
